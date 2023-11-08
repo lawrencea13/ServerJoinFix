8e1c61c1bbf8358ec58ffbd4bace64302bec785d
lawrencea13 <105841326+lawrencea13@users.noreply.github.com>
2023-11-08 13:58:43 -0500

    Uploaded mod
    added files

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TemporaryServerJoinFix
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class TempServerFix : BaseUnityPlugin
+    {
+        private const string modGUID = "Posiedon.SJF";
+        private const string modName = "Lethal Company server join fix";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        public static ManualLogSource mls;
+
+        void Awake()
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource("SJF");
+            // Plugin startup logic
+            mls.LogInfo("Loaded server join fix. Patching.");
+            harmony.PatchAll(typeof(TempServerFix));
+        }
+
+        [HarmonyPatch(typeof(GameNetworkManager), "Singleton_OnClientDisconnectCallback")]
+        [HarmonyPrefix]
+        static void FixBadServerJoin()
+        {
+            //___Instance.ResetSavedGameValues();
+            SceneManager.LoadScene("MainMenu");
+            mls.LogInfo("Loading Main Menu");
+        }
+
+    }
+}
